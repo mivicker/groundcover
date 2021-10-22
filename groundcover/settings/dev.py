cbdd7f2dd4a87a53e85ab59c03756c8e0fe875b7
mike <vickers.mike@gmail.com>
2021-10-21 23:07:45 -0400

    moved dev variables into a local folder

--- groundcover/settings/dev.py
+++ groundcover/settings/dev.py
@@ -1,19 +1,4 @@
 from .base import *
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 try:
     from .local import *
