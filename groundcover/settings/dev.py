d28c412c725e470b4fecff7f2ee3e352396cc48e
mike <vickers.mike@gmail.com>
2021-10-21 21:56:00 -0400

    clean-up commit

--- groundcover/settings/dev.py
+++ groundcover/settings/dev.py
@@ -1,13 +1,16 @@
 from .base import *
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w)21cs5a@zytx*q^d8ek@4d-tr@@u0js8euvg@qv=va7ns2n_a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
